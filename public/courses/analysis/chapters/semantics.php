51e232353dd61d209795086346408e63a60ee815
Bobby Tran <bobby-tran@email.cz>
2023-09-26 12:01:06 +0200

    Update semantics.php

--- public/courses/analysis/chapters/semantics.php
+++ public/courses/analysis/chapters/semantics.php
@@ -1,2 +1,22 @@
 <h1>Web Semantics</h1>
 
+<h2>Lecture 1 notes</h2>
+<ul>
+    <li>The field of Knowledge graphs usually uses Semantic web</li>
+    <li>Knowledge graphs - Using graphs to represent information</li>
+    <li>Web is a net of HTML documents connected via hyperlinks, you access via Browser</li>
+    <li>Linked Data - web of data instead of documents, you can browse the data and representations (still under development)</li>
+    <li>We can already insert structured annotations into HTML documents (for robots) - microformats, microdata</li>
+    <li>schema.org - initiative for Google, Yahoo etc. work together on common schemas</li>
+</ul>
+
+Semantic web
+<ul>
+    <li>We is huge, redundant, changing, semi-structured, ...</li>
+    <li>Structured information - machine readable</li>
+    <li>Semantic information - explicit information on the meaning</li>
+    <li>Semantic web - structured and linked semantic information on the web</li>
+    <li>RDF - defines the model how we represent data</li>
+    <li>Reasoner - basically what ChatGPT does</li>
+    <li>DBpedia - knowledge graph of wikipedia articles using RDF</li>
+</ul>
